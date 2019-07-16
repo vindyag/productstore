a946c3015149c3d4393f6be3ce0b5152e64c2946
SERANET\vindyag <VindyaG@99x.lk>
2019-07-16 06:46:14 +0530

    init project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+c
+3src/main/java/product/store/WebApplicationMain.java,3/a/3a557f4f75ddd99d25fc52f35ea97dffed8b8268
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/product/store/WebApplicationMain.java
@@ -0,0 +1,11 @@
+package product.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationMain {
+    public static void main(String[] args){
+        SpringApplication.run(WebApplicationMain.class, args);
+    }
+}
