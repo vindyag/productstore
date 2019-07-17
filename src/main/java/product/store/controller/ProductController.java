6beb2c485b143a822a41ddacb8f1e07af5e8158a
SERANET\vindyag <VindyaG@99x.lk>
2019-07-17 15:17:17 +0530

    fix mapping

--- src/main/java/product/store/controller/ProductController.java
+++ src/main/java/product/store/controller/ProductController.java
@@ -1,17 +1,18 @@
 package product.store.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-
 import product.store.entity.Product;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@RequestMapping(produces = {APPLICATION_JSON_VALUE})
 @RestController
 public class ProductController {
 
-    @GetMapping(value="products/")
+    @GetMapping(path = "products")
     public List<Product> getProducts() throws RuntimeException {
-        throw new RuntimeException("Not Implemented");
+        throw new RuntimeException("Not Implemented yet... :(");
     }
 }
