6b377e2515d48a3b6b1839016e675f87f44de8ac
SERANET\vindyag <VindyaG@99x.lk>
2019-07-23 13:01:37 +0530

    add swagger configurations

--- src/main/java/product/store/controller/ProductController.java
+++ src/main/java/product/store/controller/ProductController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+
+import io.swagger.annotations.ApiOperation;
 import product.store.entity.Product;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -11,6 +13,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 public class ProductController {
 
+    @ApiOperation("Product API")
     @GetMapping(path = "products")
     public List<Product> getProducts() throws RuntimeException {
         throw new RuntimeException("Not Implemented yet... :(");
